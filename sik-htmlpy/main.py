b402cdcaebfd358c2ad3703c6c3382e2b13fa2ae
Rikome <rikoz4intergrity@gmail.com>
2017-06-13 19:38:38 +0100

    Second commit

--- sik-htmlpy/main.py
+++ sik-htmlpy/main.py
@@ -1,1 +1,12 @@
-py
+import htmlPy
+import os
+
+app = htmlPy.AppGUI(title=u"SIK Test Center", maximized=True)
+
+app.template_path = os.path.abspath(".")
+app.static_path = os.path.abspath(".")
+
+app.template = ("index.html", {"username": "htmlPy_user"})
+
+if __name__ == "__main__":
+    app.start()
