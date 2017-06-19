ba4c3843894c9fc11bd7dc5ee9cc7ee6b38e2352
Rikome <rikoz4intergrity@gmail.com>
2017-06-19 15:40:33 +0100

    Fourth

--- sik-htmlpy/main.py
+++ sik-htmlpy/main.py
@@ -17,7 +17,11 @@ app.static_path = os.path.join(BASE_DIR, "static/")
 app.template_path = os.path.join(BASE_DIR, "templates/")
 
 #GUI Templates
-app.template = ("index.html", {})
+#app.template = ("index.html", {})
+#app.template = ("login.html", {})
+#app.template = ("profile.html", {})
+app.template = ("test.html", {})
+#app.template = ("submission.html", {})
 
 app.web_app.setMinimumWidth(1366)
 app.web_app.setMinimumHeight(768)
