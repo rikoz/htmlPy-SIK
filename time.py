55194e2463c0c8b90ec75bb2b94a8749790e3dc6
Rikome <rikoz4intergrity@gmail.com>
2017-08-11 07:58:16 +0100

    modal pop-up/others

--- time.py
+++
@@ -1,9 +0,0 @@
-from PySide.QtCore import QTime
-
-def test_timer():
-	msec = 3 * 60 * 1000
-	time = QtCore.QTime()
-	curr_time = time.currentTime()
-	end_time = curr_time.addMSecs(msec)
-	ret = end_time.toString("hh:mm:ss")
-return ret
